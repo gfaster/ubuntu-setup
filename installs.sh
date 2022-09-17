c75697c2abcc6ac444570d79ec7159c936b9e5bc
Gavin Rohrer <gavin@rohrer.org>
2022-09-17 13:09:10 -0400

    run as root where needed

--- installs.sh
+++ installs.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 set -e
 
-apt update
+sudo apt update
 mkdir tmp
 
 # General tools
 # --------------------
 
-apt -y install guake
+sudo apt -y install guake
 
-apt -y install neovim
+sudo apt -y install neovim
 
-apt -y install git
+sudo apt -y install git
 
-apt -y install firefox
+sudo apt -y install firefox
 
-apt -y install curl
+sudo apt -y install curl
 
 # TODO: include 1password cli (https://developer.1password.com/docs/cli/get-started/)
 snap install 1password
 
 snap install thunderbird
 
-apt -y install discord
+sudo apt -y install discord
 
 
 # Needs to be set manually in xfce
 # TODO: auto set theme, icons, and window manager theme
 curl -L https://github.com/rtlewis88/rtl88-Themes/archive/refs/tags/1.3.tar.gz > ./tmp/solarized.tar.gz
 tar -xf ./tmp/solarized.tar.gz -C ./tmp/
-tar -xf ./tmp/rtl88-Themes-1.3/solarized-dark-gtk-theme-colorpack_1.3.tar.gz -C /usr/share/themes
+sudo tar -xf ./tmp/rtl88-Themes-1.3/solarized-dark-gtk-theme-colorpack_1.3.tar.gz -C /usr/share/themes
 
 
 # Dev tools
@@ -40,11 +40,11 @@ curl -LO https://raw.githubusercontent.com/GitCredentialManager/git-credential-m
 sh ./install-from-source.sh &&
 git-credential-manager-core configure
 
-apt -y install g++
+sudo apt -y install g++
 
-apt -y install python3-pip
+sudo apt -y install python3-pip
 
-apt -y install subl
+sudo apt -y install subl
 
 
 # Cleanup
