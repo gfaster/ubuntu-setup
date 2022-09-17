d7b6b4358aaf2f19e90e4ac90f7d853e4563039b
Gavin Rohrer <gavin@rohrer.org>
2022-09-17 13:01:12 -0400

    fixes

--- configure.sh
+++ configure.sh
@@ -5,8 +5,8 @@ set -e
 sudo cp /usr/share/applications/guake.desktop /etc/xdg/autostart/
 
 # setup bash profile
-cp ./.gfasters-bashrc ~/
-grep -Fq "sh ~/.gfasters-bashrc" < ~/.bashrc || echo "sh ~/.gfasters-bashrc" >> ~/.bashrc
+cp ./.gfasters-bashrc ~/ &&
+grep -Fq "source ~/.gfasters-bashrc" < ~/.bashrc || echo "source ~/.gfasters-bashrc" >> ~/.bashrc
 
 # enable gcm - there is probably more to do here
 git config --global credential.credentialStore "cache --timeout=86400"
